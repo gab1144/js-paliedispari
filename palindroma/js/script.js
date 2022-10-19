b7f9a9435d3933d4a6f197e64a0f36e997b7874d
gab1144 <gabry.rinciari04@gmail.com>
2022-10-19 17:14:44 +0200

    [add] controllo e reset

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,17 +1,22 @@
 const el = document.querySelector.bind(document);
 const verifyButton = el('#verifica');
+const resetButton = el('#reset');
 
 verifyButton.addEventListener('click', function(){
   word= readValueById("input-word");
-  const palindromo= verifyPalindormo(word);
+  if(word !== ""){
+    const palindromo= verifyPalindormo(word);
   
-  const outputSpan= el('#output');
-  outputSpan.classList.remove('d-none');
-  
-  if (palindromo === true) {
-    outputSpan.innerText = "La parola è palindroma";
+    const outputSpan= el('#output');
+    outputSpan.classList.remove('d-none');
+    
+    if (palindromo === true) {
+      outputSpan.innerText = "La parola è palindroma";
+    } else {
+      outputSpan.innerText = "La parola non è palindroma";
+    }
   } else {
-    outputSpan.innerText = "La parola non è palindroma";
+    alert("Devi inserire una parola");
   }
 });
 
@@ -33,3 +38,8 @@ function verifyPalindormo (word) {
 
   return palindromo;
 }
+
+resetButton.addEventListener('click', function(){
+  document.getElementById('input-word').value="";
+  document.getElementById('output').classList.add('d-none');
+});
